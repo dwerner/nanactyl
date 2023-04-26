63f64b5c2f37f15ff738e17d079f484b4ec2e639
Daniel Werner <dan.werner@gmail.com>
2023-04-26 08:55:34 -0700

    cleanup

--- assets/shaders/rust_shader_builder/build.rs
+++ assets/shaders/rust_shader_builder/build.rs
@@ -13,6 +13,20 @@ fn main() {
     //     spirv-unknown-vulkan1.1
     //     spirv-unknown-vulkan1.1spv1.4
     //     spirv-unknown-vulkan1.2
+    // pub fn compile_shaders() -> Vec<SpvFile> {
+    //     SpirvBuilder::new(".", "spirv-unknown-vulkan1.1")
+    //         .print_metadata(MetadataPrintout::None)
+    //         .build()
+    //         .unwrap();
+    //         .module
+    //         .unwrap_single()
+    //         .to_path_buf();
+    // let sky_shader = SpvFile {
+    //     name: "sky_shader".to_string(),
+    //     data: read_spv(&mut File::open(sky_shader_path).unwrap()).unwrap(),
+    // };
+    // vec![sky_shader]
+    //}
     println!("cargo:rerun-if-changed=./rust_shader_builder");
     for shader in ["vertex_rustgpu", "fragment_rustgpu", "fragment_nolighting"].iter() {
         println!("cargo:rerun-if-changed=./{}", shader);
@@ -35,18 +49,3 @@ fn main() {
         .unwrap();
     }
 }
-
-// pub fn compile_shaders() -> Vec<SpvFile> {
-//     SpirvBuilder::new(".", "spirv-unknown-vulkan1.1")
-//         .print_metadata(MetadataPrintout::None)
-//         .build()
-//         .unwrap();
-//         .module
-//         .unwrap_single()
-//         .to_path_buf();
-// let sky_shader = SpvFile {
-//     name: "sky_shader".to_string(),
-//     data: read_spv(&mut File::open(sky_shader_path).unwrap()).unwrap(),
-// };
-// vec![sky_shader]
-//}
